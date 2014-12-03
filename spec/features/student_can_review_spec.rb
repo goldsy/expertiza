019d9d362733020b95909e8971c539e34fb79d68
karthiksanthanam7 <karthiksanthanam7@gmail.com>
2014-12-03 18:11:17 +0530

    updated test for review

--- spec/features/student_can_review_spec.rb
+++ spec/features/student_can_review_spec.rb
@@ -1,75 +1,42 @@
 describe 'student can review' , :type => :feature do
- it 'create a team and review' do
+ it 'create a team' do
     student1 = FactoryGirl.create :student
     student2 = FactoryGirl.create :student
     student3 = FactoryGirl.create :student
     assignment = FactoryGirl.create :assignment
-    assignment.add_participant student1.name
-    assignment.add_participant student2.name
-    assignment.add_participant student3.name
-     
-    topic = FactoryGirl.create :sign_up_topic, assignment: assignment
-
-    team_name = 'TestTeamName'
-
-    # Log in as student1
-    log_in_as_user(student1)
-
-    # Navigate to the assignment team page
-    click_link assignment.name
-    click_link 'Your team'
-
-    # Create a team
-    fill_in 'team_name', with: team_name
-    click_on 'Create Team'
-
-    # Expect team name to be displayed
-    expect(page).to have_content(team_name)
-
-    # Invite student2 to the team
-    fill_in 'user_name', with: student2.name
-    click_on 'Invite'
-
-    # Expect student2 to show up under 'Sent Invitations'
-    expect(page).to have_content(student2.name)
-
-    # Switch to student2
-    log_out
-    log_in_as_user(student2)
-
-    # Navigate to the assignment team page
-    click_link assignment.name
-    click_link 'Your team'
-
-    # Expect team name to be displayed under 'Received Invitations'
-    expect(page).to have_content(team_name)
-
-    # Accept the team invitation
-    click_link 'Accept'
-
-    # Expect team name to be displayed under 'Team members'
-    expect(page).to have_content(team_name)
-    log_out
+    #assignment.add_participant student1.name
+    #assignment.add_participant student2.name
+    #assignment.add_participant student3.name
+   # user = FactoryGirl.create :user 
+    sign_up_topic = FactoryGirl.create :sign_up_topic, assignment: assignment
+    participant1 = FactoryGirl.create :assignment_participant , user: student1 , assignment: assignment , topic_id: sign_up_topic.id # , sign_up_topic: sign_up_topic
+    participant2 = FactoryGirl.create :assignment_participant , user: student2 , assignment: assignment , topic_id: sign_up_topic.id
+    participant3 = FactoryGirl.create :assignment_participant , user: student3 , assignment: assignment , topic_id: sign_up_topic.id
  
-    # Log in with first user to create a submission
-    log_in_as_user(student1)
-    click_link assignment.name
-	  expect(page).to have_content('Your work')
-	  click_link 'Your work'
-	
-	 #fill in the submission with a valid URL and upload it
-	 fill_in 'submission', with: 'https://github.com/goldsy/expertiza'
-   click_on 'upload_link'
-   expect(page).to have_content('https://github.com/goldsy/expertiza') 
-	 log_out
-     
-	 #Log in with third user to review the submission 
-	 log_in_as_user(student3)
-   click_link assignment.name
-   expect(page).to have_content("Others' work")
-   click_link "Others' work"
-   expect(page).to have_content('topic_id_'+topic.id.to_s)
-   click_on 'Request a new submission to review'
-	 expect(page).to have_content('Begin')   
+   # team1 = FactoryGirl.create :team , parent_id: assignment.id
+   # team1.add_member(student1,assignment.id)
+   # team1.add_member(student2,assignment.id) 
+       
+    participant1.submit_hyperlink 'https://github.com/expertiza/expertiza'
+    participant2.submit_hyperlink 'https://github.com/expertiza/expertiza2'
+    participant3.submit_hyperlink 'https://github.com/expertiza'
+    log_in_as_user(student3)
+    
+     click_link assignment.name
+     #expect(page).to have_content('test')
+     expect(page).to have_content("Others' work")
+     click_link "Others' work"
+    page.choose('topic_id_'+sign_up_topic.id.to_s)     
+  #expect(page).to have_content('test')
+  #find(:css,"#i_dont_care[name='i_dont_care']").set(true)
+  # click_on 'submitStudentReviewButton'
+  click_on 'req'
+   expect(page).to have_content('test')
+  page.choose('submission_id_'+participant1.id.to_s)
+  click_on 'ReviewSelectedSubmission'
+  #expect(page).to have_content('topic_id_'+topic.id.to_s)
+  #find(:xpath,".//input[@id='topic_id_" + topic.id.to_s + "']").click
+  #click_on 'submitStudentReviewButton'
+  #expect(page).to have_content('Begin')   
  end
 end 
